e86bd43f88f35b5bd86ba7bb37e93ef4aab9cc63
bmeryu <bmeryu@gmail.com>
2025-07-29 22:32:33 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+  Archivo JavaScript para Aula Gen IA
+  
+  Aquí puedes añadir la interactividad futura del sitio web.
+  Por ejemplo:
+  - Validaciones de formularios.
+  - Animaciones al hacer scroll.
+  - Funcionalidad para un menú móvil.
+*/
+
+console.log("¡Bienvenido a Aula Gen IA! El sitio está listo para la acción.");
